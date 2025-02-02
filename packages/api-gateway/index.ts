0b23ac66344738b5094ad97b54f719ae0c7b71b4
“mmoladaa” <tangmo.n@icloud.com>
2025-02-02 22:31:21 +0700

    build: ts-node-dev

--- packages/api-gateway/index.ts
+++ packages/api-gateway/index.ts
@@ -9,6 +9,48 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("LINE MAN Wongnai Frontend Assignment"));
 
+// Endpoint to get restaurant details
+app.get("/restaurants/:restaurantId", async (req, res) => {
+  const { restaurantId } = req.params;
+  try {
+    const response = await axios.get(
+      `https://us-central1-wongnai-frontend-assignment.cloudfunctions.net/api/restaurants/${restaurantId}.json`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching restaurant data" });
+  }
+});
+
+// Endpoint to get short menu details
+app.get(
+  "/restaurants/:restaurantId/menus/:menuName/short",
+  async (req, res) => {
+    const { restaurantId, menuName } = req.params;
+    try {
+      const response = await axios.get(
+        `https://us-central1-wongnai-frontend-assignment.cloudfunctions.net/api/restaurants/${restaurantId}/menus/${menuName}/short.json`
+      );
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching short menu data" });
+    }
+  }
+);
+
+// Endpoint to get full menu details
+app.get("/restaurants/:restaurantId/menus/:menuName/full", async (req, res) => {
+  const { restaurantId, menuName } = req.params;
+  try {
+    const response = await axios.get(
+      `https://us-central1-wongnai-frontend-assignment.cloudfunctions.net/api/restaurants/${restaurantId}/menus/${menuName}/full.json`
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching full menu data" });
+  }
+});
+
 try {
   app.listen(port, (): void => {
     console.log(`Connected successfully on port ${port}`);
