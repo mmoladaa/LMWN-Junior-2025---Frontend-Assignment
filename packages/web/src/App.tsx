39dbaf542eee402f9b3571eda592d403bd247d2b
“mmoladaa” <tangmo.n@icloud.com>
2025-02-03 01:49:22 +0700

    build: install tailwindCSS

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,11 @@
+import "./index.css";
 
 function App() {
   return (
-    <div>
+    <div className="bg-red-200">
       <h1>Edit this app to complete LINE MAN Wongnai Frontend Assignment!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
